04fb018bcd162dc3134f138260dfefa1c034b78f
Andrey Shitov <a.shitov@samsung.com>
2018-01-19 17:51:55 +0300

    Add logical device and graphics queue to vulkan demo.

--- vkdemo/main.cpp
+++ vkdemo/main.cpp
@@ -95,6 +95,7 @@ private:
         createInstance();
         setupDebugCallback();
         pickPhysicalDevice();
+        createLogicalDevice();
     }
 
     void mainLoop() {
@@ -104,6 +105,7 @@ private:
     }
 
     void cleanup() {
+        vkDestroyDevice(device, nullptr);
         DestroyDebugReportCallbackEXT(instance, callback, nullptr);
         vkDestroyInstance(instance, nullptr);
 
@@ -312,12 +314,48 @@ private:
         return indices;
     }
 
+    void createLogicalDevice() {
+        QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+        VkDeviceQueueCreateInfo queueCreateInfo = {};
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily;
+        queueCreateInfo.queueCount = 1;
+
+        float queuePriority = 1.0f;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+
+        VkPhysicalDeviceFeatures deviceFeatures = {};
+
+        VkDeviceCreateInfo createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        createInfo.pQueueCreateInfos = &queueCreateInfo;
+        createInfo.queueCreateInfoCount = 1;
+        createInfo.pEnabledFeatures = &deviceFeatures;
+
+        createInfo.enabledExtensionCount = 0;
+
+        if (enableValidationLayers) {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        } else {
+            createInfo.enabledLayerCount = 0;
+        }
+
+        if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create logical device!");
+        }
+
+        vkGetDeviceQueue(device, indices.graphicsFamily, 0, &graphicsQueue);
+    }
+
 private:
     GLFWwindow* window;
     VkInstance instance;
     VkDebugReportCallbackEXT callback;
-    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
-
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE; // Physical device handle
+    VkDevice device; // Logical device handle
+    VkQueue graphicsQueue; // Queue handle
 };
 
 int main() {
